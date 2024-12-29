fc7d3bb8cefd727d581f13ec13824d5ddd3e7d60
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2024-12-28 21:37:51 -0500

    Add initial commit.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StreamCardComponent } from './stream-card/stream-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: StreamCardComponent}
+];
